be95204b70009cb91364f47f4c4d607f3b1fe0f0
a483210 <a483210@163.com>
2017-05-30 13:55:39 +0800

    添加StickerFramePlayerView文件

---
+++ stickerplayerview/src/main/java/com/xiuyukeji/stickerplayerview/StickerFramePlayerView.java
@@ -0,0 +1,626 @@
+package com.xiuyukeji.stickerplayerview;
+
+import android.content.Context;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import com.xiuyukeji.stickerplayerview.annotations.FrameRange;
+import com.xiuyukeji.stickerplayerview.annotations.FrameRateRange;
+import com.xiuyukeji.stickerplayerview.annotations.PaddingRange;
+import com.xiuyukeji.stickerplayerview.annotations.PlayerSource;
+import com.xiuyukeji.stickerplayerview.annotations.TextSizeRange;
+import com.xiuyukeji.stickerplayerview.cache.MemoryCache;
+import com.xiuyukeji.stickerplayerview.event.intefaces.OnLeftBottomListener;
+import com.xiuyukeji.stickerplayerview.event.intefaces.OnLeftTopListener;
+import com.xiuyukeji.stickerplayerview.event.intefaces.OnRightTopListener;
+import com.xiuyukeji.stickerplayerview.intefaces.OnClickStickerListener;
+import com.xiuyukeji.stickerplayerview.intefaces.OnDeleteListener;
+import com.xiuyukeji.stickerplayerview.intefaces.OnDoubleClickStickerListener;
+import com.xiuyukeji.stickerplayerview.intefaces.OnLongClickStickerListener;
+import com.xiuyukeji.stickerplayerview.intefaces.OnSelectedListener;
+import com.xiuyukeji.stickerplayerview.intefaces.OnUnselectedListener;
+import com.xiuyukeji.stickerplayerview.resource.Resource;
+
+/**
+ * 贴纸控件，以帧为单位
+ *
+ * @author Created by jz on 2017/5/30 13:35
+ */
+@UiThread
+public class StickerFramePlayerView extends FrameLayout {
+
+    private final StickerPlayerView mStickerPlayerView;
+
+    public StickerFramePlayerView(Context context) {
+        this(context, null, 0, R.style.SickerPlayerViewStyle);
+    }
+
+    public StickerFramePlayerView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0, R.style.SickerPlayerViewStyle);
+    }
+
+    public StickerFramePlayerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, R.style.SickerPlayerViewStyle);
+    }
+
+    public StickerFramePlayerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr);
+
+        mStickerPlayerView = new StickerPlayerView(context, attrs, defStyleAttr, defStyleRes);
+        addView(mStickerPlayerView);
+    }
+
+    /**
+     * 添加贴纸从from到to帧
+     *
+     * @param frame    帧
+     * @param resource 资源
+     */
+    public int addSticker(@FrameRange int frame, @NonNull Resource resource) {
+        return mStickerPlayerView.addSticker(frame, frame, resource);
+    }
+
+    /**
+     * 添加文字贴纸从from到to帧，没有背景
+     *
+     * @param frame     帧
+     * @param text      文字
+     * @param textColor 文字颜色
+     * @param textSize  文字大小
+     */
+    public int addTextSticker(@FrameRange int frame,
+                              String text, @ColorInt int textColor, @TextSizeRange int textSize) {
+        return mStickerPlayerView.addTextSticker(frame, frame,
+                text, textColor, textSize);
+    }
+
+    /**
+     * 添加文字贴纸从from到to帧，没有背景
+     *
+     * @param frame         帧
+     * @param text          文字
+     * @param textColor     文字颜色
+     * @param textSize      文字大小
+     * @param leftPadding   左边距
+     * @param topPadding    上边距
+     * @param rightPadding  右边距
+     * @param bottomPadding 下边距
+     */
+    public int addTextSticker(@FrameRange int frame,
+                              String text, @ColorInt int textColor, @TextSizeRange int textSize,
+                              @PaddingRange int leftPadding,
+                              @PaddingRange int topPadding,
+                              @PaddingRange int rightPadding,
+                              @PaddingRange int bottomPadding) {
+        return mStickerPlayerView.addTextSticker(frame, frame,
+                text, textColor, textSize,
+                leftPadding, topPadding, rightPadding, bottomPadding);
+    }
+
+    /**
+     * 添加文字贴纸从from到to帧
+     *
+     * @param frame     帧
+     * @param resource  资源
+     * @param text      文字
+     * @param textColor 文字颜色
+     * @param textSize  文字大小
+     */
+    public int addTextSticker(@FrameRange int frame, Resource resource,
+                              String text, @ColorInt int textColor, @TextSizeRange int textSize) {
+        return mStickerPlayerView.addTextSticker(frame, frame, resource,
+                text, textColor, textSize);
+    }
+
+    /**
+     * 添加文字贴纸从from到to帧
+     *
+     * @param frame         帧
+     * @param resource      资源
+     * @param text          文字
+     * @param textColor     文字颜色
+     * @param textSize      文字大小
+     * @param leftPadding   左边距
+     * @param topPadding    上边距
+     * @param rightPadding  右边距
+     * @param bottomPadding 下边距
+     */
+    public int addTextSticker(@FrameRange int frame, Resource resource,
+                              String text, @ColorInt int textColor, @TextSizeRange int textSize,
+                              @PaddingRange int leftPadding,
+                              @PaddingRange int topPadding,
+                              @PaddingRange int rightPadding,
+                              @PaddingRange int bottomPadding) {
+        return mStickerPlayerView.addTextSticker(frame, frame, resource,
+                text, textColor, textSize,
+                leftPadding, topPadding, rightPadding, bottomPadding);
+    }
+
+    /**
+     * 添加文字贴纸从from到to帧
+     *
+     * @param frame         帧
+     * @param resource      资源
+     * @param text          文字
+     * @param textColor     文字颜色
+     * @param textSize      文字大小
+     * @param delayFrame    延迟帧数显示
+     * @param leftPadding   左边距
+     * @param topPadding    上边距
+     * @param rightPadding  右边距
+     * @param bottomPadding 下边距
+     */
+    public int addTextSticker(@FrameRange int frame, Resource resource,
+                              String text, @ColorInt int textColor, @TextSizeRange int textSize,
+                              @FrameRange int delayFrame,
+                              @PaddingRange int leftPadding,
+                              @PaddingRange int topPadding,
+                              @PaddingRange int rightPadding,
+                              @PaddingRange int bottomPadding) {
+        return mStickerPlayerView.addTextSticker(frame, frame, resource,
+                text, textColor, textSize, delayFrame,
+                leftPadding, topPadding, rightPadding, bottomPadding);
+    }
+
+    /**
+     * 替换当前选中贴纸的背景
+     *
+     * @param resource 资源
+     */
+    public void replaceSticker(Resource resource) {
+        mStickerPlayerView.replaceSticker(resource);
+    }
+
+    /**
+     * 替换指定贴纸的背景
+     *
+     * @param resource 资源
+     * @param position 索引
+     */
+    public void replaceSticker(Resource resource, @FrameRange int position) {
+        mStickerPlayerView.replaceSticker(resource, position);
+    }
+
+    /**
+     * 替换当前选中文字贴纸背景
+     *
+     * @param resource      资源
+     * @param leftPadding   左边距
+     * @param topPadding    上边距
+     * @param rightPadding  右边距
+     * @param bottomPadding 下边距
+     */
+    public void replaceTextSticker(Resource resource,
+                                   @PaddingRange int leftPadding,
+                                   @PaddingRange int topPadding,
+                                   @PaddingRange int rightPadding,
+                                   @PaddingRange int bottomPadding) {
+        mStickerPlayerView.replaceTextSticker(resource, leftPadding, topPadding, rightPadding, bottomPadding);
+    }
+
+    /**
+     * 替换当前帧指定文字贴纸的背景
+     *
+     * @param resource      资源
+     * @param position      索引
+     * @param leftPadding   左边距
+     * @param topPadding    上边距
+     * @param rightPadding  右边距
+     * @param bottomPadding 下边距
+     */
+    public void replaceTextSticker(Resource resource, @FrameRange int position,
+                                   @PaddingRange int leftPadding,
+                                   @PaddingRange int topPadding,
+                                   @PaddingRange int rightPadding,
+                                   @PaddingRange int bottomPadding) {
+        mStickerPlayerView.replaceTextSticker(resource, position,
+                leftPadding, topPadding, rightPadding, bottomPadding);
+    }
+
+    /**
+     * 替换当前帧指定文字贴纸的背景
+     *
+     * @param resource      资源
+     * @param position      索引
+     * @param delayFrame    延迟帧数显示
+     * @param leftPadding   左边距
+     * @param topPadding    上边距
+     * @param rightPadding  右边距
+     * @param bottomPadding 下边距
+     */
+    public void replaceTextSticker(Resource resource, @FrameRange int position,
+                                   @FrameRange int delayFrame,
+                                   @PaddingRange int leftPadding,
+                                   @PaddingRange int topPadding,
+                                   @PaddingRange int rightPadding,
+                                   @PaddingRange int bottomPadding) {
+        mStickerPlayerView.replaceTextSticker(resource, position, delayFrame,
+                leftPadding, topPadding, rightPadding, bottomPadding);
+    }
+
+    /**
+     * 复制当前选中贴纸到当前帧
+     */
+    public void copySticker() {
+        mStickerPlayerView.copySticker();
+    }
+
+    /**
+     * 复制指定贴纸到当前帧
+     *
+     * @param position 索引
+     */
+    public void copySticker(@FrameRange int position) {
+        mStickerPlayerView.copySticker(position);
+    }
+
+    /**
+     * 复制当前选中的贴纸，从fromFrame到toFrame
+     *
+     * @param fromFrame 开始帧
+     * @param toFrame   结束帧
+     */
+    public void copySticker(@FrameRange int fromFrame, @FrameRange int toFrame) {
+        mStickerPlayerView.copySticker(fromFrame, toFrame);
+    }
+
+    /**
+     * 复制指定贴纸，从fromFrame到toFrame
+     *
+     * @param position  索引
+     * @param fromFrame 开始帧
+     * @param toFrame   结束帧
+     */
+    public void copySticker(@FrameRange int position,
+                            @FrameRange int fromFrame, @FrameRange int toFrame) {
+        mStickerPlayerView.copySticker(position, fromFrame, toFrame);
+    }
+
+    /**
+     * 复制指定贴纸，从fromFrame到toFrame
+     *
+     * @param position  索引
+     * @param dx        平移x
+     * @param dy        平移y
+     * @param fromFrame 开始帧
+     * @param toFrame   结束帧
+     */
+    public void copySticker(@FrameRange int position, int dx, int dy,
+                            @FrameRange int fromFrame, @FrameRange int toFrame) {
+        mStickerPlayerView.copySticker(position, dx, dy, fromFrame, toFrame);
+    }
+
+    /**
+     * 删除当前选中的贴纸
+     */
+    public void deleteSticker() {
+        mStickerPlayerView.deleteSticker();
+    }
+
+    /**
+     * 删除指定贴纸
+     *
+     * @param position 索引
+     */
+    public void deleteSticker(@FrameRange int position) {
+        mStickerPlayerView.deleteSticker(position);
+    }
+
+    /**
+     * 删除全部贴纸
+     */
+    public void clearAllSticker() {
+        mStickerPlayerView.clearAllSticker();
+    }
+
+    /**
+     * 获得当前选中贴纸索引
+     */
+    public int getCurrentPosition() {
+        return mStickerPlayerView.getCurrentPosition();
+    }
+
+    /**
+     * 水平翻转当前选中贴纸
+     */
+    public void flipHorizontal() {
+        mStickerPlayerView.flipHorizontal();
+    }
+
+    /**
+     * 水平翻转指定贴纸
+     *
+     * @param position 索引
+     */
+    public void flipHorizontal(@FrameRange int position) {
+        mStickerPlayerView.flipHorizontal(position);
+    }
+
+    /**
+     * 垂直翻转当前选中贴纸
+     */
+    public void flipVertical() {
+        mStickerPlayerView.flipVertical();
+    }
+
+    /**
+     * 垂直翻转指定贴纸
+     *
+     * @param position 索引
+     */
+    public void flipVertical(@FrameRange int position) {
+        mStickerPlayerView.flipVertical(position);
+    }
+
+    /**
+     * 设置当前选中贴纸的文字
+     *
+     * @param text 文字
+     */
+    public void setText(String text) {
+        mStickerPlayerView.setText(text);
+    }
+
+    /**
+     * 设置指定贴纸的文字
+     *
+     * @param text     文字
+     * @param position 索引
+     */
+    public void setText(String text, @FrameRange int position) {
+        mStickerPlayerView.setText(text, position);
+    }
+
+    /**
+     * 设置当前选中贴纸的文字颜色
+     *
+     * @param color 文字颜色
+     */
+    public void setTextColor(@ColorInt int color) {
+        mStickerPlayerView.setTextColor(color);
+    }
+
+    /**
+     * 设置指定贴纸的文字颜色
+     *
+     * @param color    文字颜色
+     * @param position 索引
+     */
+    public void setTextColor(@ColorInt int color, @FrameRange int position) {
+        mStickerPlayerView.setTextColor(color, position);
+    }
+
+    /**
+     * 设置当前选中贴纸的文字大小
+     *
+     * @param size 文字大小
+     */
+    public void setTextSize(@TextSizeRange int size) {
+        mStickerPlayerView.setTextSize(size);
+    }
+
+    /**
+     * 设置指定贴纸的文字大小
+     *
+     * @param size     文字大小
+     * @param position 索引
+     */
+    public void setTextSize(@TextSizeRange int size, @FrameRange int position) {
+        mStickerPlayerView.setTextSize(size, position);
+    }
+
+    /**
+     * 设置当前选中贴纸的文字加粗
+     *
+     * @param isBold 文字加粗
+     */
+    public void setBold(boolean isBold) {
+        mStickerPlayerView.setBold(isBold);
+    }
+
+
+    /**
+     * 设置指定贴纸的文字加粗
+     *
+     * @param isBold   文字加粗
+     * @param position 索引
+     */
+    public void setBold(boolean isBold, @FrameRange int position) {
+        mStickerPlayerView.setBold(isBold, position);
+    }
+
+    /**
+     * 设置当前选中贴纸的文字倾斜
+     *
+     * @param isItalic 文字倾斜
+     */
+    public void setItalic(boolean isItalic) {
+        mStickerPlayerView.setItalic(isItalic);
+    }
+
+    /**
+     * 设置指定贴纸的文字倾斜
+     *
+     * @param isItalic 文字倾斜
+     * @param position 索引
+     */
+    public void setItalic(boolean isItalic, @FrameRange int position) {
+        mStickerPlayerView.setItalic(isItalic, position);
+    }
+
+    /**
+     * 设置当前选中贴纸的文字下划线
+     *
+     * @param isUnderline 文字下划线
+     */
+    public void setUnderline(boolean isUnderline) {
+        mStickerPlayerView.setUnderline(isUnderline);
+    }
+
+    /**
+     * 设置指定贴纸的文字下划线
+     *
+     * @param isUnderline 文字下划线
+     * @param position    索引
+     */
+    public void setUnderline(boolean isUnderline, @FrameRange int position) {
+        mStickerPlayerView.setUnderline(isUnderline, position);
+    }
+
+    /**
+     * 获取当前帧
+     */
+    public int getCurrentFrame() {
+        return mStickerPlayerView.getCurrentFrame();
+    }
+
+    /**
+     * 设置当前时间，最终会被转化为帧
+     *
+     * @param timeMs 时间，单位毫秒
+     */
+    public void setCurrentTime(@IntRange(from = 0) int timeMs) {
+        mStickerPlayerView.setCurrentTime(timeMs);
+    }
+
+    /**
+     * 设置当前帧
+     *
+     * @param frameIndex 帧
+     */
+    public void setCurrentFrame(@FrameRange int frameIndex) {
+        mStickerPlayerView.setCurrentFrame(frameIndex);
+    }
+
+    /**
+     * 设置帧率(fps)，区间{1-60}
+     *
+     * @param frameRate 帧率
+     */
+    public void setFrameRate(@FrameRateRange int frameRate) {
+        mStickerPlayerView.setFrameRate(frameRate);
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        mStickerPlayerView.setEnabled(enabled);
+    }
+
+    /**
+     * 设置播放器状态，编辑或者播放
+     *
+     * @param state {@link PlayerSource#EDIT} and {@link PlayerSource#PLAYER}
+     */
+    public void setPlayerState(@PlayerSource int state) {
+        mStickerPlayerView.setPlayerState(state);
+    }
+
+    /**
+     * 设置内存管理，必须在没有添加贴纸之前设置
+     *
+     * @param memoryCache 内存管理
+     */
+    public void setMemoryCache(MemoryCache memoryCache) {
+        mStickerPlayerView.setMemoryCache(memoryCache);
+    }
+
+    /**
+     * 暂停播放，在{@link PlayerSource#EDIT}下有效
+     */
+    public void pause() {
+        mStickerPlayerView.pause();
+    }
+
+    /**
+     * 恢复播放，在{@link PlayerSource#EDIT}下有效
+     */
+    public void resume() {
+        mStickerPlayerView.resume();
+    }
+
+    /**
+     * 左上按钮回调
+     *
+     * @param l 回调
+     */
+    public void setOnLeftTopListener(OnLeftTopListener l) {
+        mStickerPlayerView.setOnLeftTopListener(l);
+    }
+
+    /**
+     * 右上按钮回调
+     *
+     * @param l 回调
+     */
+    public void setOnRightTopListener(OnRightTopListener l) {
+        mStickerPlayerView.setOnRightTopListener(l);
+    }
+
+    /**
+     * 左下按钮回调
+     *
+     * @param l 回调
+     */
+    public void setOnLeftBottomListener(OnLeftBottomListener l) {
+        mStickerPlayerView.setOnLeftBottomListener(l);
+    }
+
+    /**
+     * 删除贴纸时回调
+     *
+     * @param l 回调
+     */
+    public void setOnDeleteListener(OnDeleteListener l) {
+        mStickerPlayerView.setOnDeleteListener(l);
+    }
+
+    /**
+     * 单击贴纸时回调，第二次单击会和doubleClick事件互斥
+     *
+     * @param l 回调
+     */
+    public void setOnClickStickerListener(OnClickStickerListener l) {
+        mStickerPlayerView.setOnClickStickerListener(l);
+    }
+
+    /**
+     * 双击贴纸时回调，第一下会触发单击
+     *
+     * @param l 回调
+     */
+    public void setOnDoubleClickStickerListener(OnDoubleClickStickerListener l) {
+        mStickerPlayerView.setOnDoubleClickStickerListener(l);
+    }
+
+    /**
+     * 长按贴纸时回调
+     *
+     * @param l 回调
+     */
+    public void setOnLongClickStickerListener(OnLongClickStickerListener l) {
+        mStickerPlayerView.setOnLongClickStickerListener(l);
+    }
+
+    /**
+     * 选择贴纸时回调
+     *
+     * @param l 回调
+     */
+    public void setOnSelectedListener(OnSelectedListener l) {
+        mStickerPlayerView.setOnSelectedListener(l);
+    }
+
+    /**
+     * 取消下载贴纸时回调，如果选中新的贴纸，会回调旧的贴纸
+     *
+     * @param l 回调
+     */
+    public void setOnUnselectedListener(OnUnselectedListener l) {
+        mStickerPlayerView.setOnUnselectedListener(l);
+    }
+}
